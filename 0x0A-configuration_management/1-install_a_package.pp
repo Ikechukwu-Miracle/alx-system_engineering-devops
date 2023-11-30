5815d9d89266e0e1a30a2e9a2f1244ae2a629d58
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-11-30 03:04:32 +0000

    reverted to a previous commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install Flask version 2.1.0 using pip3
-
- exec { 'flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+#
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
