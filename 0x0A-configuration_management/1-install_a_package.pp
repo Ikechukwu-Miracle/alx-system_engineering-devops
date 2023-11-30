e78443751c41dafa1b5d404450ca412488b684a1
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-11-30 02:53:13 +0000

    fixed for puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install Flask version 2.1.0 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+ exec { 'flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
