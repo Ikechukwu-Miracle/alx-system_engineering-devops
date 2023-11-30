d5cefb330ad58757e62c98ae006ad5374596d913
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-11-30 02:45:48 +0000

    fixed for puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kill the process named 'killmenow' using pkill
 exec { 'killmenow_process':
-	command => 'pkill killmenow',
-	provider => 'shell',
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
 
