8c21536fb3e7bb65c23bd605c5a6c8eeb339d6bb
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-01-17 05:08:25 +0100

    created readme and 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a function that querries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+
+
+def number_of_subscribers(subreddit):
+    """Querries the Reddit API and returns
+    the number of subscribers for a given subreddit
+    """
+    import requests
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "myUserAgent"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code >= 300:
+        return 0
+
+    subscribers = response.json().get("data").get("subscribers")
+    return subscribers
